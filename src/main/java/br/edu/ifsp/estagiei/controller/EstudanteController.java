d63a3fd71cb1d8cab00ecddbd8b7e4bd9c360e29
Leonardo Marques <leomarques301@gmail.com>
2022-05-05 04:47:49 -0300

    fix: Modifica busca de estudante

--- src/main/java/br/edu/ifsp/estagiei/controller/EstudanteController.java
+++ src/main/java/br/edu/ifsp/estagiei/controller/EstudanteController.java
@@ -1,27 +1,28 @@
 package br.edu.ifsp.estagiei.controller;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.edu.ifsp.estagiei.model.Estudante;
-import br.edu.ifsp.estagiei.repositorio.EstudanteRepositorio;
+import br.edu.ifsp.estagiei.entity.Estudante;
+import br.edu.ifsp.estagiei.repository.StudentRepository;
 
 @RestController
 public class EstudanteController {
 	
 	@Autowired
-	EstudanteRepositorio dao;
+	private StudentRepository repositorio;
 	
-	@GetMapping(path = "/estudante")
+	@GetMapping(path = "api/estudante/{id}")
 	@ResponseStatus(value = HttpStatus.OK)
-	public ResponseEntity<List<Estudante>> listarEstudantes(){
-		List<Estudante> estudantes = dao.listarEstudantes(); 
-		return ResponseEntity.ok(estudantes);
+	public ResponseEntity<Estudante> getEstudante(@PathVariable String id){
+		Optional<Estudante> estudante = repositorio.findById(id);
+		return ResponseEntity.ok(estudante.get());
 	}
 }
