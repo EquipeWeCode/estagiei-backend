975593fa6d52e96e7aca81f391fca37c9ce8d875
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2022-05-05 07:39:41 -0300

    fix: criacao da tabela de estudantes

---
+++ src/main/resources/db/migration/V1__create_estudante_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tb_estudante (
+    cod_estudante VARCHAR NOT NULL,
+    nome VARCHAR,
+    dt_nasc DATE,
+    rg VARCHAR,
+    cpf VARCHAR,
+    nivel_escolaridade VARCHAR,
+    experiencia_profissional VARCHAR
+)
--- src/main/resources/db/migration/V1__dummy_migration.sql
+++
@@ -1,3 +0,0 @@
-spring.flyway.table=service1_flyway_schema_history
-spring.flyway.baseline-on-migrate=true
-spring.flyway.baseline-version=0
