aec6de40470101aadee548b9a25927244f336ff3
Igor Nathan <igor.natan.355@gmail.com>
2022-05-14 21:51:21 -0300

    Criação das tabelas necessárias para a POC - Dados de exemplo

--- src/main/resources/db/migration/V1__create_estudante_table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE tb_estudante (
-    cod_estudante VARCHAR NOT NULL,
-    nome VARCHAR,
-    dt_nasc DATE,
-    rg VARCHAR,
-    cpf VARCHAR,
-    nivel_escolaridade VARCHAR,
-    experiencia_profissional VARCHAR
-)
