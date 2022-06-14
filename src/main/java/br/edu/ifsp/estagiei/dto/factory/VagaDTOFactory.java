d7e0a05838bd33718b068d34f41818d4fb735dd9
Igor Nathan <igor.natan.355@gmail.com>
2022-06-14 20:47:48 -0300

    fix: Problemas ao realizar o merge com o VagaDTOFactory

--- src/main/java/br/edu/ifsp/estagiei/dto/factory/VagaDTOFactory.java
+++ src/main/java/br/edu/ifsp/estagiei/dto/factory/VagaDTOFactory.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import br.edu.ifsp.estagiei.dto.CompetenciaDTO;
 import br.edu.ifsp.estagiei.dto.VagaDTO;
 import br.edu.ifsp.estagiei.dto.builder.VagaDTOBuilder;
 import br.edu.ifsp.estagiei.entity.Competencia;
@@ -16,46 +17,41 @@ import lombok.NoArgsConstructor;
 @Component
 @NoArgsConstructor
 public class VagaDTOFactory {
-	
+
+	@Autowired
+	CompetenciaDTOFactory competenciaFactory;
+
 	public Set<Vaga> buildVagas(List<VagaDTO> dto) {
 		return dto.stream().map(this::buildEntity).collect(Collectors.toSet());
 	}
-	
+
 	public Vaga buildEntity(VagaDTO dto) {
 		Vaga vaga = new Vaga();
 		vaga.setCodVaga(dto.getCodVaga());
 		vaga.setDescricao(dto.getDescricao());
 		vaga.setTitulo(dto.getTitulo());
 		vaga.setSalario(dto.getSalario());
-		//vaga.setEmpresa(empresaFactory.buildEntity(dto.getEmpresa()));
-		//vaga.setCompetencias(this.buildEntities(dto.getCompetencias());
+		// vaga.setEmpresa(empresaFactory.buildEntity(dto.getEmpresa()));
+		// vaga.setCompetencias(this.buildEntities(dto.getCompetencias());
 		vaga.setIndAtivo(dto.getIndAtivo());
 		return vaga;
 	}
-	
+
 	public List<VagaDTO> buildLista(Set<Vaga> vagas) {
-	@Autowired
-	CompetenciaDTOFactory competenciaFactory;
-	
-	public List<VagaDTO> buildLista(List<Vaga> vagas) {
 		return vagas.stream().map(this::buildVaga).collect(Collectors.toList());
 	}
-	
+
 	public VagaDTO buildVaga(Vaga vaga) {
 		VagaDTOBuilder builder = VagaDTOBuilder.newInstance();
-		
-				builder
-				//.empresa(vaga.getEmpresa())
-				.codVaga(vaga.getCodVaga())
-				.descricao(vaga.getDescricao())
-				.titulo(vaga.getTitulo())
-				.indAtivo(vaga.getIndAtivo())
-				.salario(vaga.getSalario());
-				
-				Set<Competencia> competencias = vaga.getCompetencias().stream() //TODO: Precisa fazer o fetch join para conseguir pegar esses dados
-					.map(c -> c.getCompetencia()).collect(Collectors.toSet());
-				builder.competencias(buildCompetencias(competencias));
-			
+
+		builder
+				// .empresa(vaga.getEmpresa())
+				.codVaga(vaga.getCodVaga()).descricao(vaga.getDescricao()).titulo(vaga.getTitulo())
+				.indAtivo(vaga.getIndAtivo()).salario(vaga.getSalario());
+
+		Set<Competencia> competencias = vaga.getCompetencias().stream().collect(Collectors.toSet());
+		builder.competencias(competenciaFactory.buildCompetencias(competencias));
+
 		return builder.build();
 	}
 
@@ -69,20 +65,15 @@ public class VagaDTOFactory {
 		dto.setDescricaoCompetencia(comp.getDescricao());
 		return dto;
 	}
-	
+
 	public List<Competencia> buildEntities(List<CompetenciaDTO> dtos) {
 		return dtos.stream().map(this::buildEntity).collect(Collectors.toList());
 	}
-	
+
 	public Competencia buildEntity(CompetenciaDTO dto) {
 		Competencia entidade = new Competencia();
 		entidade.setCodCompetencia(dto.getCodCompetencia());
 		entidade.setDescricao(dto.getDescricaoCompetencia());
 		return entidade;
-	}
-				Set<Competencia> competencias = vaga.getCompetencias().stream().collect(Collectors.toSet());
-				builder.competencias(competenciaFactory.buildCompetencias(competencias));
-			
-		return builder.build();
 	}
 }
