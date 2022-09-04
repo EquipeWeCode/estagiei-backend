a0c530736bfba964be78276b8d794ec83bdf2d2c
Leonardo Marques <leomarques301@gmail.com>
2022-09-03 23:03:25 -0300

    fix: Faz o fetch do usuário sempre que buscar uma empresa

--- src/main/java/br/edu/ifsp/estagiei/repository/EmpresaRepository.java
+++ src/main/java/br/edu/ifsp/estagiei/repository/EmpresaRepository.java
@@ -1,13 +1,18 @@
 package br.edu.ifsp.estagiei.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import br.edu.ifsp.estagiei.entity.Empresa;
 
 public interface EmpresaRepository extends CrudRepository<Empresa, Long> {
+	
+	@Query("SELECT emp FROM Empresa emp LEFT JOIN FETCH emp.usuario WHERE emp.codEmpresa = :codEmpresa")
+	public Optional<Empresa> findById(@Param("codEmpresa") Long codEmpresa);
 	
 	@Query("SELECT DISTINCT emp FROM Empresa emp LEFT JOIN FETCH emp.endereco endereco LEFT JOIN FETCH emp.usuario")
 	public List<Empresa> findAll();
