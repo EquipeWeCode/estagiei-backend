ea14cad0b0765ed470c67fd05915d852933fe3d0
Leonardo Marques <leomarques301@gmail.com>
2022-09-10 17:29:37 -0300

    fix: Merge

--- src/main/java/br/edu/ifsp/estagiei/controller/EstudanteControllerImpl.java
+++ src/main/java/br/edu/ifsp/estagiei/controller/EstudanteControllerImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -43,9 +44,11 @@ public class EstudanteControllerImpl implements EstudanteController {
 	}
 
 	@Override
+	@PostMapping
 	public ResponseEntity<EstudanteDTO> postEstudante(@RequestBody @Valid EstudanteDTO dto) {
-		// TODO Auto-generated method stub
-		return null;
+		EstudanteDTO estudante = service.salvaEstudante(dto);
+		return ResponseEntity.ok(estudante);
+
 	}
 
 	@PutMapping(PATH_ID)
