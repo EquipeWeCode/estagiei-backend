1b16ba7a2857e4a0616883c1ce6ebe738632b5b2
Igor Nathan <igor.natan.355@gmail.com>
2022-10-01 22:12:51 -0300

    Commit Feat - Adiciona serviço para atualizar processo de candidatura,  modifica path params, do metodo de post.

--- src/main/java/br/edu/ifsp/estagiei/controller/CandidaturaController.java
+++ src/main/java/br/edu/ifsp/estagiei/controller/CandidaturaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+import br.edu.ifsp.estagiei.constants.CandidaturaEnum;
 import br.edu.ifsp.estagiei.dto.CandidaturaDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -17,6 +18,10 @@ public interface CandidaturaController extends Controller {
 
 	@ApiResponse(responseCode = "201")
 	@Operation(summary = "Inicia um novo processo de candidatura de um estudante", tags = { CANDIDATURA })
-	public ResponseEntity<CandidaturaDTO> postCandidatura(CandidaturaDTO dto);
+	public ResponseEntity<CandidaturaDTO> postCandidatura(Long codEstudante, Long codVaga);
+	
+	@ApiResponse(responseCode = "201")
+	@Operation(summary = "Atualiza o status da candidatura", tags = { CANDIDATURA })
+	public ResponseEntity<CandidaturaDTO> putCandidatura(CandidaturaDTO dto);
 
 }
