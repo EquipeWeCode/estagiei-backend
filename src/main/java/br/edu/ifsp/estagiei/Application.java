c012d6834d174de7a1a92f8d5f60ceb882f5f97c
Leonardo Marques <leomarques301@gmail.com>
2022-10-14 23:09:42 -0300

    fix: Adiciona bean de e-mail

--- src/main/java/br/edu/ifsp/estagiei/Application.java
+++ src/main/java/br/edu/ifsp/estagiei/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -37,5 +39,10 @@ public class Application extends SpringBootServletInitializer {
 			}
 		};
 	}
+	
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
 
 }
