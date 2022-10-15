146cfc95da051581972b941ccbe1a94b3aac0384
Leonardo Marques <leomarques301@gmail.com>
2022-10-15 11:50:34 -0300

    feat: Adiciona endpoint de exclusão de candidaturas

--- src/main/java/br/edu/ifsp/estagiei/controller/CandidaturaControllerImpl.java
+++ src/main/java/br/edu/ifsp/estagiei/controller/CandidaturaControllerImpl.java
@@ -7,14 +7,17 @@ import javax.validation.Valid;
 import org.springdoc.api.annotations.ParameterObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifsp.estagiei.constants.CandidaturaEnum;
@@ -49,7 +52,7 @@ public class CandidaturaControllerImpl implements CandidaturaController {
 		dto.setCodEstudante(codEstudante);
 		dto.setCodVaga(codVaga);
 		dto.setStatus(CandidaturaEnum.CANDIDATADO);
-		
+
 		CandidaturaDTO candidatura = service.salvaCandidatura(dto, false);
 		return ResponseEntity.ok(candidatura);
 	}
@@ -61,6 +64,13 @@ public class CandidaturaControllerImpl implements CandidaturaController {
 	public ResponseEntity<CandidaturaDTO> putCandidatura(@RequestBody @Valid CandidaturaDTO dto) {
 		CandidaturaDTO candidatura = service.salvaCandidatura(dto, true);
 		return ResponseEntity.ok(candidatura);
+	}
+
+	@Override
+	@DeleteMapping(PATH_ID)
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteCandidatura(@PathVariable Long codEstudante, @PathVariable Long codVaga) {
+		service.excluiCandidatura(codEstudante, codVaga);
 	}
 
 }
