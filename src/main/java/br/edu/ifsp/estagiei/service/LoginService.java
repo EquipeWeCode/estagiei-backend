af1554e80ded7693f69196e864c35b06d01697e4
Leonardo Marques <leomarques301@gmail.com>
2022-11-16 00:50:05 -0300

    fix: Corrige retorno da expiracao jwt

--- src/main/java/br/edu/ifsp/estagiei/service/LoginService.java
+++ src/main/java/br/edu/ifsp/estagiei/service/LoginService.java
@@ -22,13 +22,16 @@ public class LoginService {
 	@Autowired
 	private JwtTokenUtil jwtUtil;
 
+	public static final long EXPIRE_DURATION = 180 * 60000; // 10.800.000; 180min
+
 	public LoginResponseDTO montaAutenticacao(Authentication authentication) {
 		Usuario user = (Usuario) authentication.getPrincipal();
 		String accessToken = jwtUtil.generateAccessToken(user);
 
 		Set<Permissao> permissoes = user.getPermissoes();
 		List<String> permissoesString = permissoes.stream().map(p -> p.toString()).collect(Collectors.toList());
-		LoginResponseDTO dto = new LoginResponseDTO(accessToken, permissoesString, JwtTokenUtil.EXPIRE_DURATION);
+		LoginResponseDTO dto = new LoginResponseDTO(accessToken, permissoesString,
+				System.currentTimeMillis() + EXPIRE_DURATION);
 
 		return dto;
 	}
